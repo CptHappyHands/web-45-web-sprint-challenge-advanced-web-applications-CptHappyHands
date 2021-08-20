7d7eb529a6448021dbf39bf1afef2b06d96235ac
Andrew Cummings <andrew.amalee@gmail.com>
2021-08-20 14:03:22 -0700

    did as much of testing as I could remember

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Color from './Color';
+import Color from "./Color";
 
 test("Renders without errors with blank color passed into component", () => {
+  render(<Color color="" />);
 });
-  
+
 test("Renders the color passed into component", () => {
+  // render(<Color color={green}/>)
+  // const testGreen = screen.
 });
 
-test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
-});
+test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {});
 
-test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
-    
-});
+test("Executes setEditColor and toggleEdit property when color div is clicked", () => {});
